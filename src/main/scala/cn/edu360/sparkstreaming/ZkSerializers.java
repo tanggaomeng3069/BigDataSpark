9e444ff30f54f95a47cc59e54533eae250a6cd2e
tanggaomeng <2647437068@qq.com>
2020-09-23 14:57:56 +0800

    sparkstreaming kafka wordcount2

---
+++ src/main/scala/cn/edu360/sparkstreaming/ZkSerializers.java
@@ -0,0 +1,22 @@
+package cn.edu360.sparkstreaming;
+
+import org.I0Itec.zkclient.exception.ZkMarshallingError;
+import org.I0Itec.zkclient.serialize.ZkSerializer;
+
+/**
+ * Author: tanggaomeng
+ * Date: 2020/9/23 14:37
+ * Describe:
+ */
+public class ZkSerializers implements ZkSerializer {
+
+    //序列化，数据--》byte[]
+    public byte[] serialize(Object o) throws ZkMarshallingError {
+        return String.valueOf(o).getBytes();
+    }
+    //反序列化，byte[]--->数据
+    public Object deserialize(byte[] bytes) throws ZkMarshallingError {
+        return new String(bytes);
+    }
+
+}
