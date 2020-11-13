0fef24cdddf22e011520e46468fd28e630362b31
tanggaomeng <2647437068@qq.com>
2020-11-13 11:01:12 +0800

    scala chapter01

--- src/main/scala/cn/edu360/shangguigu/hanshunping/scala/chapter01/vars/VarDemo03.scala
+++ src/main/scala/cn/edu360/shangguigu/hanshunping/scala/chapter01/vars/VarDemo03.scala
@@ -1,12 +1,13 @@
 package cn.edu360.shangguigu.hanshunping.scala.chapter01.vars
 
 object VarDemo03 {
-  var name = "hello"
-  val age = 100
-  def main(args: Array[String]): Unit = {
-    println("ok")
+    var name = "hello"
+    val age = 100
 
-  }
+    def main(args: Array[String]): Unit = {
+        println("ok")
+
+    }
 }
 
 
