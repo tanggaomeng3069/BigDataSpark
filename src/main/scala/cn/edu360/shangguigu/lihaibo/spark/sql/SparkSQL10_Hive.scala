81834b0438d6e1d29682d6ae44f9d7f7f2f2de50
tanggaomeng <2647437068@qq.com>
2021-01-25 10:18:10 +0800

    Spark SQL

--- src/main/scala/cn/edu360/shangguigu/lihaibo/spark/sql/SparkSQL10_Hive.scala
+++ src/main/scala/cn/edu360/shangguigu/lihaibo/spark/sql/SparkSQL10_Hive.scala
@@ -11,6 +11,7 @@ import org.apache.spark.sql.SparkSession
 object SparkSQL10_Hive {
 
     def main(args: Array[String]): Unit = {
+        System.setProperty("HADOOP_USER_NAME", "root")
 
         val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL")
         val spark: SparkSession = SparkSession
@@ -20,12 +21,20 @@ object SparkSQL10_Hive {
           .getOrCreate()
         import spark.implicits._
 
-        // TODO 使用SparkSQL连接外置的Hive
-        //  1.拷贝hive-site.xml文件到classpath下
-        //  2.启动hive的支持
-        //  3.增加对应的依赖关系（包含MySQL驱动）
+        // TODO 使用SparkSQL连接内置的Hive
+        //  1.启动hive的支持
+        //  2.增加对应的依赖关系（包含MySQL驱动）
 
-        spark.sql("show tables").show()
+//        spark.sql("show tables").show()
+//        spark.sql("create table aa(id int)")
+//        spark.sql("show tables").show()
+
+//        spark.sql("load data local inpath 'input/id.txt' into table aa")
+//        spark.sql("select * from aa").show()
+
+        // TODO 如果连接外置的Hive，需要把Hive on Tez去掉
+        //  目前还不知道如何在Ambari中去掉Hive on Tez 修改为 Hive on Spark
+        spark.sql("show databases").show()
 
 
         spark.close()
